33c61b3f014b1958cc3bd289a5482df5bf42ed51
loaiza474 <79678955+loaiza474@users.noreply.github.com>
2021-12-18 14:34:52 -0500

    Add files via upload

---
+++ backend/src/main/java/com/example/reto2/CrudRepository/OrderRepository.java
@@ -0,0 +1,27 @@
+package com.example.reto2.CrudRepository;
+
+
+import java.time.LocalDateTime;
+
+import java.util.List;
+// import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+// import org.springframework.data.mongodb.repository.Query;
+
+
+import com.example.reto2.Models.Documents.Order;
+// import com.example.reto2.Models.Documents.User;
+
+public interface OrderRepository extends MongoRepository<Order, Integer>{
+
+	
+	public List<Order> findBySalesManZone(String salesMan);
+	
+	public List<Order> findBySalesManIdAndStatus(Integer id, String status);
+	
+	//@Query("{'registerDay': 0? , 'salesMan._id':1? }")
+	public List<Order> findByRegisterDayAndSalesManId(LocalDateTime registerDay, Integer id);
+	
+	public List<Order> findBySalesManId(Integer id);
+}
---
+++ backend/src/main/java/com/example/reto2/Services/OrderService.java
@@ -0,0 +1,86 @@
+package com.example.reto2.Services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.reto2.CrudRepository.OrderRepository;
+import com.example.reto2.Models.Documents.Order;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	OrderRepository orderRepo;
+	
+	public Order save(Order order) {
+		Optional<Order> e = orderRepo.findById(order.getId());
+		//Actualiza si ya existe el producto 
+		if(e.isPresent()) {
+			Order actua = new Order();
+			actua.setId(e.get().getId());
+			actua.setProducts(e.get().getProducts());
+			actua.setStatus(order.getStatus());
+			actua.setQuantities(e.get().getQuantities());
+			actua.setRegisterDay(e.get().getRegisterDay());
+			actua.setSalesMan(e.get().getSalesMan());
+			return orderRepo.save(actua);
+		}
+		else {
+			//Guarda un producto nuevo
+			if (order.getId() != null) {
+				return orderRepo.save(order);
+			}
+			return order;
+		}
+        
+		
+        
+	}
+	
+	 // listar todos los Orders
+    public List<Order> getOrders() {
+        return orderRepo.findAll();
+    }
+    
+  	//validar por ID
+    public Optional<Order> getOrder(int order) {
+        return orderRepo.findById(order);
+    }
+	
+    //find by zone
+    public List<Order> findZone(String zone) {
+		return orderRepo.findBySalesManZone(zone);
+    }
+    
+    //find by fecha y id
+    public List<Order> findIdAndDate(String registerDay, Integer id ) {
+
+		//Instancia de objeto para formatear la fecha tomada del endpoint
+		DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		//Instancia de objeto tipo fecha local para hacer coincidir la fecha en la maquina de producción
+		LocalDateTime localDate =  LocalDate.parse(registerDay, formatoFecha).atStartOfDay();
+		
+		return orderRepo.findByRegisterDayAndSalesManId(localDate, id);
+    }
+    
+    //find by status y id
+    public List<Order> findIdAndStatus(Integer id, String status) {
+    	
+		return orderRepo.findBySalesManIdAndStatus(id, status);
+    }
+    
+    //find by idUser
+    public List<Order> findIdUser(Integer id) {
+    	
+		return orderRepo.findBySalesManId(id);
+    }
+}
